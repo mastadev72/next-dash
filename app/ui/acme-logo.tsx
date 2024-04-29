55b204c34a7ff517cca5d2e974eb529faa3cf4ae
ed <devstuv@gmail.com>
2024-04-29 20:22:34 +0200

    second commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,17 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import {
+  GlobeAltIcon,
+  BoltIcon,
+  LightBulbIcon,
+} from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/ui/fonts'
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <LightBulbIcon className='h-10 w-10 rotate-[10deg]' />
+      <p className='text-[44px]'>TankSlim</p>
     </div>
-  );
+  )
 }
