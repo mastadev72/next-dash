3029b23219fe8ee203e1cd92c760d2dae549fc97
ed <devstuv@gmail.com>
2024-04-29 21:54:24 +0200

    4 commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} antialiased`}>
+      <body className={`${inter.className} antialiased bg-slate-300`}>
         {children}
         <SpeedInsights />
       </body>
